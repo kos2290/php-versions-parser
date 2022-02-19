b94d644b18195251e0d3b79400b96cb901723ad0
Curtis Jackson <kos28032@gmail.com>
2022-02-19 15:06:40 +0600

    Add base PHP class

---
+++ src/PHPVersionsParser.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KoS\PHPVersionsParser;
+
+use DOMDocument;
+
+/**
+ * Class PHPVersionsParser
+ */
+class PHPVersionsParser
+{
+    /**
+     * Returns current stable version of the PHP from php.net website
+     *
+     * @return string
+     */
+    public function getCurrentStableVersion(): string
+    {
+        $content = file_get_contents('https://www.php.net/downloads');
+
+        libxml_use_internal_errors(true);
+        $DOM = new DOMDocument;
+        $DOM->loadHTML($content);
+        libxml_use_internal_errors(false);
+
+        $items = $DOM->getElementsByTagName('h3');
+
+        $version = '';
+        for ($i = 0; $i < $items->length; $i++) {
+            if (false !== strpos($items->item($i)->nodeValue, 'Current Stable')
+                && $items->item($i)->getAttribute('id')
+            ) {
+                $version = str_replace('v', '', $items->item($i)->getAttribute('id'));
+                break;
+            }
+        }
+
+        return $version;
+    }
+}
